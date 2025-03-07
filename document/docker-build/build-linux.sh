e7363d9115119ef7b4fa4e03006e47aacab8505b
dairo-ths <1064348912@qq.com>
2025-03-07 17:21:20 +0900

    添加版本号

--- document/docker-build/build-linux.sh
+++ document/docker-build/build-linux.sh
@@ -42,33 +42,34 @@ if [ ! -e $exec_file ]; then
 fi
 
 
-##---------------------------------------创建标签----------------------------------------
-## 获取版本号
-#version=$(grep -oP '(?<=VERSION = ")[^"]+' main.go)
-#
-##删除本地已经存在的标签
-#git tag -d $version
-#
-##删除远程标签
-#git push origin --delete tag $version
-#
-#git tag $version
-#git push origin $version
-#
-#release_message="本次发布版本:$version"
-#create_release_api_response=$(curl -L -X POST "https://api.github.com/repos/$repo/releases" \
-#                        -H "Accept: application/vnd.github.v3+json" \
-#                        -H "Authorization: Bearer $github_token" \
-#                        -H "X-GitHub-Api-Version: 2022-11-28" \
-#                        -d "{\"tag_name\":\"$version\",\"name\":\"$version\",\"body\":\"$release_message\"}")
-#echo "创建标签结果:${create_release_api_response}"
-#
-##通过正则匹配ReleaseId, [head -n 1]功能是从匹配到的多个字符串中去第一个字符串
-#release_id=$(echo "$create_release_api_response" | grep -oP '(?<="id": )[^,]+' | head -n 1)
-#echo "本地发布ID:${release_id}"
-#
-#
-##---------------------------------------上传编译好的二进制文件----------------------------------
+#---------------------------------------创建标签----------------------------------------
+# 获取版本号
+version=$(grep -oP '(?<=VERSION = ")[^"]+' application/Application.go)
+echo "获取到版本号:$version"
+
+#删除本地已经存在的标签
+git tag -d $version
+
+#删除远程标签
+git push origin --delete tag $version
+
+git tag $version
+git push origin $version
+
+release_message="本次发布版本:$version"
+create_release_api_response=$(curl -L -X POST "https://api.github.com/repos/$repo/releases" \
+                        -H "Accept: application/vnd.github.v3+json" \
+                        -H "Authorization: Bearer $github_token" \
+                        -H "X-GitHub-Api-Version: 2022-11-28" \
+                        -d "{\"tag_name\":\"$version\",\"name\":\"$version\",\"body\":\"$release_message\"}")
+echo "创建标签结果:${create_release_api_response}"
+
+#通过正则匹配ReleaseId, [head -n 1]功能是从匹配到的多个字符串中去第一个字符串
+release_id=$(echo "$create_release_api_response" | grep -oP '(?<="id": )[^,]+' | head -n 1)
+echo "本地发布ID:${release_id}"
+
+
+#---------------------------------------上传编译好的二进制文件----------------------------------
 #upload_file_api_response=$(curl -s -X POST \
 #                            -H "Accept: application/vnd.github+json" \
 #                            -H "Authorization: Bearer ${github_token}" \
