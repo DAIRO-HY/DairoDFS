e2f912b9755200ad3e60d6172b6b6ab3acee06d9
dairo-ths <1064348912@qq.com>
2025-03-21 16:13:34 +0900

    修改编译环境

--- document/docker-runtime/build.bat
+++ document/docker-runtime/build.bat
@@ -1,1 +1,1 @@
-docker build --no-cache -t dairopapa/dairo-dfs-runtime:1.0.0 .
+docker build --no-cache -t dairo-dfs-runtime .
--- document/docker-runtime/build.sh
+++ document/docker-runtime/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --no-cache -t dairopapa/dairo-dfs-runtime:1.0.0 .
+docker build --no-cache -t dairo-dfs-runtime .
