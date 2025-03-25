d6b06b60db437e4d31ccdf11e6112130b0044ce3
dairo-ths <1064348912@qq.com>
2025-03-25 20:26:37 +0900

    修复BUG：修复文件同步失败的BUG

--- util/DistributedUtil/StorageFileSyncHandle/StorageFileSyncHandle.go
+++ util/DistributedUtil/StorageFileSyncHandle/StorageFileSyncHandle.go
@@ -6,10 +6,8 @@ import (
 	"DairoDFS/util/DfsFileUtil"
 	"DairoDFS/util/DistributedUtil"
 	"DairoDFS/util/DistributedUtil/SyncDownloadUtil"
-	"fmt"
 	"io"
 	"os"
-	"reflect"
 	"strings"
 )
 
@@ -72,40 +70,32 @@ func download(info *DistributedUtil.SyncServerInfo, md5 string, masterStorageFil
 // 移动文件到数据目录
 func moveFile(src string, target string) {
 
-	fmt.Println("-->01")
-
 	//移动文件
 	moveErr := os.Rename(src, target)
 	if moveErr == nil {
 		return
 	}
-	fmt.Println("-->1:", reflect.TypeOf(moveErr))
 
 	//不同的盘符之间不能使用Rename操作
-	if !strings.HasSuffix(moveErr.Error(), "The system cannot move the file to a different disk drive.") {
-		fmt.Println("-->2:" + moveErr.Error())
+	//windows报错：The system cannot move the file to a different disk drive.
+	//linux报错：invalid cross-device link
+	if !strings.HasSuffix(moveErr.Error(), "The system cannot move the file to a different disk drive.") && !strings.HasSuffix(moveErr.Error(), "invalid cross-device link") {
 		panic(moveErr)
 	}
-	fmt.Println("-->3:" + moveErr.Error())
 	source, _ := os.Open(src)
-	fmt.Println("-->4")
 	defer source.Close()
 	targetFile, _ := os.Create(target)
 	defer targetFile.Close()
-	fmt.Println("-->5")
 	if _, err := io.Copy(targetFile, source); err != nil {
 		os.Remove(target)
 		panic(err)
 	}
-	fmt.Println("-->6")
 
 	//确保文件已经写入到了磁盘，避免突然断电导致文件数据丢失
 	if err := targetFile.Sync(); err != nil {
 		os.Remove(target)
 		panic(err)
 	}
-	fmt.Println("-->7")
 	source.Close()
 	os.Remove(src)
-	fmt.Println("-->8")
 }
