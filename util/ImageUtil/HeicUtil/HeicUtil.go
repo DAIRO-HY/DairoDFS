c7580c38f838f4388234254c3e614cff23dbdc25
dairo-ths <1064348912@qq.com>
2025-04-07 19:21:13 +0900

    解决无法获取heic格式图片的拍摄日期BUG

--- util/ImageUtil/HeicUtil/HeicUtil.go
+++ util/ImageUtil/HeicUtil/HeicUtil.go
@@ -1,13 +1,14 @@
 package HeicUtil
 
 import (
+	"DairoDFS/extension/String"
 	"DairoDFS/util/ImageUtil"
 	"DairoDFS/util/ShellUtil"
 )
 
 // 生成图片缩略图
 func Thumb(path string, targetMaxSize int) ([]byte, error) {
-	pngData, err := ToPng(path)
+	pngData, err := ToJpeg(path, 100)
 	if err != nil {
 		return nil, err
 	}
@@ -21,16 +22,12 @@ func ToPng(path string) ([]byte, error) {
 
 // 转换成JPEG图片
 func ToJpeg(path string, quality int8) ([]byte, error) {
-	pngData, err := ToPng(path)
-	if err != nil {
-		return nil, err
-	}
-	return ImageUtil.Png2Jpg(pngData, quality)
+	return ShellUtil.ExecToOkData("magick \"" + path + "\" -quality " + String.ValueOf(quality) + " JPEG:-")
 }
 
 // 获取图片信息
 func GetInfo(path string) (ImageUtil.ImageInfo, error) {
-	data, err := ToPng(path)
+	data, err := ToJpeg(path, 1)
 	if err != nil {
 		return ImageUtil.ImageInfo{}, err
 	}
