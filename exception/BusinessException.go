e2ad30c87a261243ba236579af0a9bfadc4d8c65
zhoulq <615741631@qq.com>
2025-09-01 16:42:19 +0900

    异常发生时,将异常内容转JSON字符串

--- exception/BusinessException.go
+++ exception/BusinessException.go
@@ -1,6 +1,8 @@
 package exception
 
-import "fmt"
+import (
+	"encoding/json"
+)
 
 // 定义一个自定义错误类型
 type BusinessException struct {
@@ -10,7 +12,8 @@ type BusinessException struct {
 
 // 实现 error 接口中的 Error() 方法
 func (e *BusinessException) Error() string {
-	return fmt.Sprintf("Error %d: %s", e.Code, e.Msg)
+	jsonData, _ := json.Marshal(e)
+	return string(jsonData)
 }
 
 // 获取一个异常
